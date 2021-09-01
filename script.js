f63b6bac12f3a6747408a0e0af45bc1420edfdac
Viktoria <vika.perova.88.88@mail.ru>
2021-09-01 12:49:55 +0300

    Интерполяция

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const typeGoods = 'food';
+
+// console.log('https://market.com/'+ typeGoods);
+
+// console.log('https://market.com/'+ typeGoods + '/'+ '10');
+
+console.log(`https://market.com/${typeGoods}`);
