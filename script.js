a43cadc470d6660603312b79123896a1350633df
Viktoria <vika.perova.88.88@mail.ru>
2021-09-07 08:51:41 +0300

    js3

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 "use strict";
 
-const typeGoods = 'food';
+let number = 10;
 
-// console.log('https://market.com/'+ typeGoods);
+function showMessage(text) {
+    console.log(text);
+    //let number = 20;
+    console.log(number);
+}
 
-// console.log('https://market.com/'+ typeGoods + '/'+ '10');
+showMessage("Hello!!!");
+console.log(number);
 
-console.log(`https://market.com/${typeGoods}`);
+const server = function(){
+    console.log('server start...');
+};
+
+server();
+
+const calc = (x, y) => x+y;
+// const calc = (x, y) => {
+//    return x+y
+// };
+
+// const calc = x => x+10;
+
+console.log(calc(10,5));
+
+
+const Hello = () => {
+    console.log("Hello!");
+    return Math.PI;
+};
+
+console.log(Hello());
+
