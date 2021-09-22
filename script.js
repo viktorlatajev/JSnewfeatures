83a767ff443a41ba85f039b49ad2ad07285fb176
Viktoria <vika.perova.88.88@mail.ru>
2021-09-22 12:27:10 +0300

    js3

--- script.js
+++ script.js
@@ -1,5 +1,63 @@
 "use strict";
 
+const person = {
+    name: 'Alex',
+    age: 28
+}
+
+// Такой формат мы уже можем отправлять на сервер
+let toServer = JSON.stringify(person);
+
+console.log(toServer);
+
+let fromServer = JSON.parse(toServer);
+console.log(fromServer);
+
+// Глубокое клонирование объекта
+const clone = JSON.parse(JSON.stringify(person));
+clone.name = 'Ann';
+console.log(person);
+console.log(clone);
+
+// Задание
+const obj = {
+    scale: {
+        width: 200,
+        height: 250
+    },
+    color: 'red',
+    shape: 'circle',
+    name: 'name1',
+    number: 1,
+    property: 'property1'
+}
+
+let toServer = JSON.stringify(obj);
+console.log(toServer);
+
+let fromServer = JSON.parse(toServer);
+console.log(fromServer);
+
+/* XML
+<?xml version="1.0" encoding="UTF-8"?>
+<obj>
+   <color>red</color>
+   <name>name1</name>
+   <number>1</number>
+   <property>property1</property>
+   <scale>
+      <height>250</height>
+      <width>200</width>
+   </scale>
+   <shape>circle</shape>
+</obj>
+*/
+
+let objClone = JSON.parse(JSON.stringify(obj));
+objClone.color = 'blue';
+
+console.log(obj);
+console.log(objClone); 
 // promise похожи на call-back функции
 
 //пример разрастания дерева call-back-ов (call-back hell)
@@ -192,18 +250,3 @@ test(2000).then(() => console.log('2000 ms'));
 Promise.all([test(4000), test(1000)]).then(() => {
     console.log('All');
 }); 
-// https://jsonplaceholder.typicode.com/ API
-
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json));
-
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: "POST",
-    body: JSON.stringify({name: "Alex"}),
-    headers: {
-        'Content-type': 'application/json'
-    }
-})
-  .then(response => response.json())
-  .then(json => console.log(json)); 
